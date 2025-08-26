402f9e68a49ea068a0da90e3899769cf4d71cd6c
Arpita Mandal <arpita03mandal@gmail.com>
2025-08-26 20:14:23 +0530

    Create size of an array.cpp

---
+++ size of an array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int size=5;
+    int marks[size];
+    for (int i=0;i<size;i++) {
+        cin>>marks[i];
+        cout<<marks[i]<<endl;
+    }
+    return 0;
+}
